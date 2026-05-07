98df0a0820fc0d14fb606e09edc1fc5c3405b266
AliEmredk <aliemreuzunoglu@gmail.com>
2026-05-07 13:25:45 +0200

    to merge

--- server/Tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
+++ server/Tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f72e7b59ed5f4708d7d7d272d1afb1e1cd18486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5b5bcfee03b23385b5dfb53182c7e23489e54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
