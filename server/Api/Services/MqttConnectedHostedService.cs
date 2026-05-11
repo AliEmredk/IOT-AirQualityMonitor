429fecf62ef2a78739e93ff9f77c026ac2bf32ef
katja981 <katstr01@easv365.dk>
2026-05-11 11:32:30 +0200

    Small changes for making Mqtt listener work

---
+++ server/Api/Services/MqttConnectedHostedService.cs
@@ -0,0 +1,23 @@
+﻿using Mqtt.Controllers;
+
+namespace api.Services;
+
+public class MqttConnectHostedService(
+    IMqttClientService mqtt,
+    ILogger<MqttConnectHostedService> logger
+) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await mqtt.ConnectAsync(
+            "mqtt.flespi.io",
+            1883,
+            "YOUR_FLESPI_TOKEN",
+            ""
+        );
+
+        logger.LogInformation("✅ MQTT connected to mqtt.flespi.io");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
