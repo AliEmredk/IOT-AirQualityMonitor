3f5770f0b140fc452785fdc0e7d7d1a0ac7190b3
katja981 <katstr01@easv365.dk>
2026-05-18 16:07:47 +0200

    Added intial logic for showing data

---
+++ server/Api/Models/TelemetryGraphDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models;
+
+public class TelemetryGraphDto
+{
+    public DateTimeOffset Timestamp { get; set; }
+    public double? GasAnalogValue { get; set; }
+    public double? TemperatureC { get; set; }
+    public double? HumidityPercent { get; set; }
+    public double? PressureHpa { get; set; }
+}
